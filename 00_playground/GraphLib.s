67454475fe7f8cb37349013c3fd1c27ee42818fc
Thomas Haug <thomas.haug@mathema.de>
2018-01-31 15:47:26 +0100

    added new functions and some sample code

--- 00_playground/GraphLib.s
+++ 00_playground/GraphLib.s
@@ -11,6 +11,12 @@
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; beginregion graph engine related procedures
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;; graph storage engine related procedures
 
 ;; structure:
@@ -24,6 +30,13 @@
 ;; alternative design 1: 
 ;; alternative design 2: storage egnine defines procdures and there is a dispatch procedure)
 
+;; choosen design: A graph database is a list with the following content
+;;(name next-vertex-id! next-edge-id! (add-vertex) (add-edge))
+;; where name            is a symbol
+;;       next-vertex-id! is a procedure returns the next vertrex id. It has no arguments
+;;       next-edge-id!   is a procedure returns the next vertrex id. It has no arguments
+;;       (list of vertex procedure)
+;;       (list of edge procedure)
 
 (define (engine-next-vertex-id engine)
   ((cadr engine)))
@@ -32,7 +45,23 @@
   ((caddr engine)))
 
 (define (engine-add-vertex! engine vertex)
-  ((cadddr engine) vertex))
+  ((car (engine-vertex-procedures engine)) vertex))
+
+(define (engine-vertex-count engine)
+  ((cadr (engine-vertex-procedures engine))))
+
+(define (engine-vertices engine)
+  ((caddr (engine-vertex-procedures engine))))
+
+;; convenience procedure to retrieve the list of available vertex procedures of the graph engine
+(define (engine-vertex-procedures engine)
+  (cadddr engine))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; endregion graph engine related procedures
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;; simple in-memory engine implementation
@@ -49,15 +78,23 @@
   (let* ((id 0)
         (next-id! (lambda () (begin (set! id (+ id 1)) id)))
         (vertices '())
-        (add-vertex (lambda (vertex) (set! vertex (cons vertex vertices))))
+        (add-vertex (lambda (vertex) (set! vertices (cons vertex vertices))))
+        (count-vertices (lambda () (length vertices)))
+        (get-vertices (lambda () vertices))   ;; is this correct?
         )
-    (list name next-id! next-id! add-vertex))
+    (list name next-id! next-id! (list add-vertex count-vertices get-vertices) '()))
 )
 
 
 (define (create-graph name engine)
  (list name engine '() '())
   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; beginregion VERTEX related procedures
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Create a Vertex with a label
 ;; the vertex has no properties 
@@ -81,6 +118,16 @@
 (define (vertex-set-property! vertex name value)
   (hashtable-set! (vertex-properties vertex) name value))
 
-;aaa
+;;; endregion VERTEX related procedures
 
-;;(define (add-vertex! graph label
+
+;;; region sample code
+
+
+; (define g (create-in-memory-engine 'Test))
+; (define v (engine-create-vertex g 'TestVertex))
+; (engine-vertex-count g)
+; (engine-add-vertex! g v)
+; (engine-vertex-count g)
+; (engine-vertices g)
+;;;
